4f92d76285f1183b09975cd0aaac1778affb5ee5
TomasPerez1 <101677692+TomasPerez1@users.noreply.github.com>
2023-07-27 13:43:20 -0300

    developing lenguage metod

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import Spline from "@splinetool/react-spline";
+import en from "../text_content";
 
 function App() {
   return (
@@ -6,12 +7,11 @@ function App() {
       <div className="">
         <h1 className="border-2 border-yellow-500">Hola beba</h1>
       </div>
-      <div className="grid gap-4 grid-cols-3 border-2 border-red-400">
-        <p>A</p>
-        <p>b</p>
-        <p>b</p>
-        <p>b</p>
-        <p>b</p>
+      <div className="grid gap-2 grid-cols-3 border-2 border-red-400">
+        <p>{en.side_nav.skills}</p>
+        <p>{en.side_nav.about}</p>
+        <p>{en.side_nav.proyects}</p>
+
         {/*       <Spline scene="https://prod.spline.design/A002HD6f1vPCzyki/scene.splinecode" />
          */}
       </div>
---
+++ src/text_content.js
@@ -0,0 +1,41 @@
+const en = {
+  side_nav : {
+    about : 'about',
+    skills: 'skills',
+    proyects: 'proyects',
+    contact: 'Contact me'
+  },
+  about : {
+    en : '',
+    es: '',
+  },
+  about : {
+    en : '',
+    es: '',
+  },
+  about : {
+    en : '',
+    es: '',
+  },
+}
+
+const esp = {
+  side_nav : {
+    en : '',
+    es: '',
+  },
+  about : {
+    en : '',
+    es: '',
+  },
+  about : {
+    en : '',
+    es: '',
+  },
+  about : {
+    en : '',
+    es: '',
+  },
+}
+
+export default en
