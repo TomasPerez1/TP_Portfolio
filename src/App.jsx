feab41518e13737fe6e096e4aff148d2591f9072
TomasPerez1 <101677692+TomasPerez1@users.noreply.github.com>
2023-07-27 12:55:55 -0300

    learning Grid

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,15 @@ function App() {
       <div className="">
         <h1 className="border-2 border-yellow-500">Hola beba</h1>
       </div>
-      <div className='w-fit'>
-      <Spline scene="https://prod.spline.design/A002HD6f1vPCzyki/scene.splinecode" />
-      </div>
+      <div className='grid gap-4 grid-cols-3 border-2 border-red-400'>
+      <p>A</p>
+      <p>b</p>
+      <p>b</p>
+      <p>b</p>
+      <p>b</p>
+      <p>b</p>
+{/*       <Spline scene="https://prod.spline.design/A002HD6f1vPCzyki/scene.splinecode" />
+ */}  </div>
     </>
   )
 }
