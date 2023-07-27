90360e2425b915b88027f7a04ab61ba18f9f1161
TomasPerez1 <101677692+TomasPerez1@users.noreply.github.com>
2023-07-27 15:13:45 -0300

    more text

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import Spline from "@splinetool/react-spline";
-import en from "../text_content";
+import {en, esp} from "../text_content";
 
 function App() {
   return (
@@ -8,7 +8,7 @@ function App() {
         <h1 className="border-2 border-yellow-500">Hola beba</h1>
       </div>
       <div className="grid gap-2 grid-cols-3 border-2 border-red-400">
-        <p>{en.side_nav.skills}</p>
+        <p>{esp.side_nav.skills}</p>
         <p>{en.side_nav.about}</p>
         <p>{en.side_nav.proyects}</p>
 
--- src/text_content.js
+++ src/text_content.js
@@ -5,37 +5,38 @@ const en = {
     proyects: 'proyects',
     contact: 'Contact me'
   },
-  about : {
-    en : '',
-    es: '',
+  about_me : {
+    text: 'I am a Full Stack developer with experience working with Node js, React, Redux, SQL amongother technologies in the industry. With ability to manage a clear decision making, lead my team to the objectives set, generate effective problem solving and identify the customer orientation. Passionate about sports, technology.',
   },
-  about : {
-    en : '',
-    es: '',
+  contact : {
+    title: 'Contame sending an email'
   },
-  about : {
-    en : '',
-    es: '',
+  proyects : {
+    coutribary: 'My first proyect, where i incorporate my knowledge adquired in my bootcamp, I learned basic CSS in a few weeks',
+    properties: 'With a group of 7 Devs, we create a an app to publish, rent and sell properties, contacting with the direct owners od the property',
+    hydroteck: 'With 2 more amazing dev we create an Ecommerce with advanced features, a high professional design and an integrated blog'
   },
 }
 
 const esp = {
   side_nav : {
-    en : '',
-    es: '',
+    about : 'about',
+    skills: 'habilidades',
+    proyects: 'proyects',
+    contact: 'Contact me'
   },
-  about : {
-    en : '',
-    es: '',
+  about_me : {
+    text: 'I am a Full Stack developer with experience working with Node js, React, Redux, SQL amongother technologies in the industry. With ability to manage a clear decision making, lead my team to the objectives set, generate effective problem solving and identify the customer orientation. Passionate about sports, technology.',
   },
-  about : {
-    en : '',
-    es: '',
+  contact : {
+    title: 'Contame sending an email'
   },
-  about : {
-    en : '',
-    es: '',
+  proyects : {
+    coutribary: 'My first proyect, where i incorporate my knowledge adquired in my bootcamp, I learned basic CSS in a few weeks',
+    properties: 'With a group of 7 Devs, we create a an app to publish, rent and sell properties, contacting with the direct owners od the property',
+    hydroteck: 'With 2 more amazing dev we create an Ecommerce with advanced features, a high professional design and an integrated blog'
   },
 }
 
-export default en
+
+export {en, esp}
