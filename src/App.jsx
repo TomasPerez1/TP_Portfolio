1d4855a300a97bebc04d6611a1fdb9262eb14fc7
TomasPerez1 <101677692+TomasPerez1@users.noreply.github.com>
2023-08-04 12:25:57 -0300

    maquetacion

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+//import Spline from "@splinetool/react-spline";
+import {en, esp} from "./text_content";
+//import {SideNav, Toggle, ContactMe, Skills} from "./components"
+import SideNav from "./components/SideNav"
+import Toggle from "./components/Toggle";
+import ContactMe from "./components/ContactMe";
+import Skills from "./components/Skills";
+
+
+function App() {
+  return (
+    <div>
+      <SideNav/>
+       {/* <Toggle/>
+      <ContactMe/>
+      <Skills/>  */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SideNav.jsx
@@ -0,0 +1,5 @@
+export default function SideNav() {
+  return(
+    <div><h1>aaa</h1></div>
+  )
+}
