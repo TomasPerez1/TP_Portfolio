7a76a5d87a8c0fa53ba607d6d515f5966066a526
TomasPerez1 <101677692+TomasPerez1@users.noreply.github.com>
2023-08-04 11:55:33 -0300

    finally a color palette

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,16 @@
-import Spline from "@splinetool/react-spline";
+//import Spline from "@splinetool/react-spline";
 import {en, esp} from "../text_content";
 
 function App() {
   return (
     <>
-      <div className="">
+      <div className="bg-accent">
         <h1 className="border-2 border-yellow-500">Hola beba</h1>
       </div>
       <div className="grid gap-2 grid-cols-3 border-2 border-red-400">
         <p>{esp.side_nav.skills}</p>
         <p>{en.side_nav.about}</p>
         <p>{en.side_nav.proyects}</p>
-
-        {/*       <Spline scene="https://prod.spline.design/A002HD6f1vPCzyki/scene.splinecode" />
-         */}
       </div>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        primary: '#176072',
-        secondary: '#d5dff6',
-        'bg-main': '#ffffff',
-        text: '#040201',
-        accent: '#060d1e',
-        'bg-two': '#f7f7f7',
+        text: "#0b0713",
+        background: "#f3f0fa",
+        primary: "#6f42bd",
+        secondary: "#dbcdee",
+        accent: "#261640",
+        textDark: "#f3f0fa",
+        backgroundDark: "#0b0713",
+        primaryDark: "#6f42bd",
+        secondaryDark: "#10091a",
+        accentDark: "#6a3db2",
       },
     },
   },
-  plugins: [require('tailwind-scrollbar')],
-}
-
+  plugins: [require("tailwind-scrollbar")],
+};
